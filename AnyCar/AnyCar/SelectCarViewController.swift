2f6f958a6593315b58a99baef299adc00519f52f
David Stancu <David@Charcoal.home>
2016-04-11 21:06:56 -0400

    initial commit

---
+++ AnyCar/AnyCar/SelectCarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnyCar
+//
+//  Created by David Stancu on 4/11/16.
+//  Copyright © 2016 Awesome IT. All rights reserved.
+//
+
+import UIKit
+
+class SelectCarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
