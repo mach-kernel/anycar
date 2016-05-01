d8d7f3f5c8d17fcb0337a5ddb91f506bf5230f2a
Paulina Levit <levit@nyu.edu>
2016-05-01 19:54:55 -0400

    lots of updates + gifs + distance + better sounds

--- AnyCar/AnyCar/SelectCarViewController.swift
+++ AnyCar/AnyCar/SelectCarViewController.swift
@@ -10,25 +10,48 @@ import UIKit
 
 class SelectCarViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
-    var cars: [String:Array<String>] = [
-        "Audi R8" : ["r8.jpg", "r8.mp3"],
-        "Jetsons" : []
+    var cars: [String:String] = [
+        "Audi R8" : "r8",
+        "Jetsons" : "jetsons"
     ]
     
     @IBOutlet var CarPicker: UIPickerView!
+    @IBOutlet var unitModal: UISegmentedControl!
+    @IBOutlet var unitSampleLabel: UILabel!
+    @IBOutlet var unitSample: UILabel!
+    @IBOutlet var unitSampleSlider: UISlider!
+    @IBOutlet var unitCeiling: UILabel!
+    @IBOutlet var infoButton: UIButton!
+    
     var pickerData : Array<String>!
     
     @IBOutlet var MPH: UISegmentedControl!
+    
+    @IBAction func sampleChanged(sender: AnyObject) {
+        self.unitSample.text = "\(self.unitSampleSlider.value)"
+    }
+    
+    @IBAction func unitsChanged(sender: AnyObject) {
+        let isImperial = self.unitModal.selectedSegmentIndex == 0
+        
+        self.unitSampleLabel.text = isImperial ? "mph" : "kmh"
+        self.unitCeiling.text = isImperial ? "mph" : "kmh"
+        
+        // Approximation
+        self.unitSampleSlider.maximumValue = isImperial ? 160.0 : 257.0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         CarPicker.dataSource = self
         CarPicker.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
+        let infoButtonImage = UIImage(contentsOfFile: "info.png")
+        self.infoButton.setBackgroundImage(infoButtonImage, forState: .Normal)
     }
     
     override func viewWillAppear(animated: Bool) {
         self.pickerData = Array(self.cars.keys)
+        self.unitSample.text = "\(self.unitSampleSlider.value)"
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,19 +71,22 @@ class SelectCarViewController: UIViewController,UIPickerViewDataSource,UIPickerV
         return pickerData[row]
     }
     
+    @IBAction func infoButtonTapped(sender: UIButton) {
+
+        let alert = UIAlertController(title: "Sampling Speed", message: "Choose a top speed for your activity! (suggested: 6mph for walking/jogging, 25mph for biking, 100mph for driving)", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         
         let carName = pickerData[CarPicker.selectedRowInComponent(0)]
-        let carAudioLink = cars[carName]![1]
-        let carImageLink = cars[carName]![0]
     
         let destinationVC = segue.destinationViewController as! DriveViewController
         destinationVC.carName = carName
-        destinationVC.carImageLink = carImageLink
-        destinationVC.carSoundLink = carAudioLink
-//        // Create a new variable to store the instance of PlayerTableViewController
-//        let destinationVC = segue.destinationViewController as PlayerTableViewController
-//        destinationVC.programVar = newProgramVar
+        destinationVC.carAssetKey = cars[carName]
+        destinationVC.sampleSpeed = Double(self.unitSampleSlider.value)
+        destinationVC.isImperial = self.unitModal.selectedSegmentIndex == 0
     }
 }
 
